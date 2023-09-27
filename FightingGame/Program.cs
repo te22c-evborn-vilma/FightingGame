9987d96c292203690c42dca3be2cecbabb686865
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2023-09-27 09:52:12 +0200

    change

--- FightingGame/Program.cs
+++ FightingGame/Program.cs
@@ -3,15 +3,18 @@
 Console.ForegroundColor = ConsoleColor.Black;
 Console.BackgroundColor = ConsoleColor.White;
 
-Console.WriteLine("A is fighting B");
+Console.WriteLine("Welcome to my fighting game!");
+Console.WriteLine("You are fighting someone");
 
 int hp1 = 100;
-string char1 = "A";
+string char1 = "You";
 
 int hp2 = 100;
-string char2 = "B";
+string char2 = "Someone";
 
 Random generator = new Random();
+
+Console.WriteLine($"\n{char1} is fighting {char2}");
 
 while (hp1 > 0 && hp2 > 0)
 {
@@ -20,19 +23,19 @@ while (hp1 > 0 && hp2 > 0)
   Console.WriteLine($"{char1}: {hp1}hp  {char2}: {hp2}hp");
   
   Console.ForegroundColor = ConsoleColor.DarkCyan;
-  int aDMG = generator.Next(15);
-  hp2 -= aDMG;
+  int char1DMG = generator.Next(25);
+  hp2 -= char1DMG;
   hp2 = Math.Max(0, hp2);
-  Console.WriteLine($"{char1} does {aDMG} DMG on {char2}");
+  Console.WriteLine($"{char1} does {char1DMG} DMG on {char2}");
 
   Console.ForegroundColor = ConsoleColor.DarkRed;
-  int bDMG = generator.Next(15);
-  hp1 -= bDMG;
+  int char2DMG = generator.Next(25);
+  hp1 -= char2DMG;
   hp1 = Math.Max(0, hp1);
-  Console.WriteLine($"{char2} does {bDMG} DMG on {char1}");
+  Console.WriteLine($"{char2} does {char2DMG} DMG on {char1}");
 
   Console.ForegroundColor = ConsoleColor.Black;
-  Console.WriteLine("Press any button to continue");
+  Console.WriteLine("Press any button to continue the fight");
   Console.ReadKey();
 }
 
@@ -41,17 +44,17 @@ Console.WriteLine("\nThe fight is over");
 
 if (hp1 == 0 && hp2 == 0)
 {
-  Console.WriteLine("The fight was tied");
+  Console.WriteLine("It's a tie!");
 }
 else if (hp1 == 0)
 {
   Console.ForegroundColor = ConsoleColor.DarkRed;
-  Console.WriteLine($"The winner is {char2}");
+  Console.WriteLine($"The winner is {char2}!");
 }
 else
 {
   Console.ForegroundColor = ConsoleColor.DarkCyan;
-  Console.WriteLine($"The winner is {char1}");
+  Console.WriteLine($"The winner is {char1}!");
 }
 
 Console.ForegroundColor = ConsoleColor.Black;
